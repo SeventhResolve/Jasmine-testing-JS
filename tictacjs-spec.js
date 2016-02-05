78d334ef0f007e0d1b6f1d504e2684e01a60a8c4
Hackbright Student <info@hackbrightacademy.com>
2016-02-05 12:53:30 -0800

    Complete step two

--- tictacjs-spec.js
+++ tictacjs-spec.js
@@ -13,12 +13,16 @@ describe("Tic Tac JS", function () {
     it("should detect a non-full board as not full", function () {
        board = ["X", "X", null, "X", "X", "X", "X", "X", "X"];
        // Add a matcher for this test.
+       expect(isBoardFull()).toBeFalsy();
+
     });
     
     it("should convert x, y to cell #", function () {
        var cellNumber1 = xyToCell(1, 1);
        var cellNumber2 = xyToCell(0, 0);
        // Add 2 matchers for this test, one for cellNumber1 and one for cellNumber2.
+        expect(cellNumber1).toBe(4);
+        expect(cellNumber2).toBe(0);
     });
     
     it("should make computer moves", function () {
@@ -26,12 +30,18 @@ describe("Tic Tac JS", function () {
        computerMove();
        var newBoardPiece = board[1];
        // Add a matcher for this test.
+       expect(newBoardPiece).toBe("O");
     });
 
     // END STEP 2
 
     // Add the following tests, or think of some of your own.
-    // it should update the board correctly based on the board
+    // it should update the board correctly based on the board variable
+    it("should update the board correctly based on the board", function () {
+
+       // Add a matcher for this test.
+
+    });
     // it should not fill in board slots that are already occupied
     // it should be able to determine a winner
     // it should not determine a winner when there is a tie
